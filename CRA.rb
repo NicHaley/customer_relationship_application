52d811e6ffbccf82e6356032442708e822633508
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-10-24 15:55:45 -0400

    Major updates to CRA.rb

--- CRA.rb
+++ CRA.rb
@@ -14,9 +14,9 @@ class CRM
     puts "[1] Add a new contact"
     puts "[2] Modify an existing contact"
     puts "[3] Delete a contact"
-    puts "[4] Display one contact"
+    puts "[4] Search by ID"
     puts "[5] Display all the contacts"
-    puts "[6] Display an attribute"
+    puts "[6] Show attributes"
     puts "[7] Exit"
     puts "Enter a number: "
   end
@@ -44,27 +44,6 @@ class CRM
     exit if user_selected == 7
   end
 
-  def id_selector
-    puts "Do you have a contact ID?"
-    puts "[1] Yes"
-    puts "[2] No"
-    user_mod_select = gets.chomp.to_i
-
-    if user_mod_select == 1
-      print "Please enter ID now: "
-      id_input = gets.chomp.to_i
-      spec_contact = @rolodex.contacts[id_input]
-      puts "You have selected:"
-      puts "#{spec_contact.first_name} #{spec_contact.last_name}"
-      puts "Email: #{spec_contact.email}"
-      puts "Notes: #{spec_contact.note}"
-    elsif user_mod_select == 2
-      puts "Please refer to contact directory:"
-      display_contacts
-    end 
-    id_input
-  end
-
   def add_new_contact
     print "Enter First Name: "
     first_name = gets.chomp
@@ -80,7 +59,7 @@ class CRM
   end
 
   def modify_contact
-    id_input = id_selector
+    id_input = display_a_contact
     puts "Please enter new information:"
     print "Enter First Name: "
       @rolodex.contacts[id_input].first_name = gets.chomp
@@ -93,9 +72,38 @@ class CRM
   end
 
   def delete_contact
+    id_input = display_a_contact
+    puts "Are you sure you would like to delete this contact?"
+    puts "[1] Yes"
+    puts "[2] No"
+    user_mod_select = gets.chomp.to_i
+    if user_mod_select == 1
+      puts "Contact deleted"
+      @rolodex.contacts.delete_at(id_input)
+    elsif user_mod_select == 2
+      puts "Contact delete cancelled"
+    end
   end
 
   def display_a_contact
+    puts "Do you have a contact ID?"
+    puts "[1] Yes"
+    puts "[2] No"
+    user_select = gets.chomp.to_i
+
+    if user_select == 1
+      print "Please enter ID now: "
+      id_input = gets.chomp.to_i
+      spec_contact = @rolodex.contacts[id_input]
+      puts "You have selected:"
+      puts "#{spec_contact.first_name} #{spec_contact.last_name}"
+      puts "Email: #{spec_contact.email}"
+      puts "Notes: #{spec_contact.note}"
+    elsif user_select == 2
+      puts "Please refer to contact directory:"
+      display_contacts
+    end 
+    id_input
   end
 
   def display_contacts
@@ -108,8 +116,20 @@ class CRM
   end
 
   def display_attributes
+    puts "What would you like to display?"
+    puts "[1] First name"
+    puts "[2] Last name"
+    puts "[3] Email"
+    puts "[4] Notes"
+    user_select = gets.chomp.to_i
+    (search = first_name) if user_select == 1
+    (search = last_name) if user_select == 2
+    (search = email) if user_select == 3
+    (search = notes) if user_select == 4
+    @rolodex.contacts.each do |contact|
+      puts "#{contact.search}"
+    end
   end
-
 end
 
 crm = CRM.new("Bitmaker")
